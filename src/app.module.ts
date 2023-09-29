e61401f0ad6f212e730b173fe0da5275019922d1
dianaM <diana.montano@assuresoftbolivia.com>
2023-09-29 16:40:49 -0400

    Add email module for email confirmation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         cache: false
       }),
     }),
+    MailModule,
   ],
   controllers: [AppController],
   providers: [AppService],
